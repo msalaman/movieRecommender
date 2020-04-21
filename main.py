337697e284115f1831b27084e9bf4b09876a7f30
alane3 <alane3@nd.edu>
2020-04-21 13:33:26 -0400

    main.py starter code

---
+++ main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3.8
+
+''' 
+    main.py
+
+    CSE 40437 Final Project
+    Abby Lane, Marcos Salamanca, Jack Meyer
+
+    Give movie rec(s) based on user's recent tweets
+'''
+
+
+# twitter token info
+
+# 1. Take in user, return most positive tweet(s)
+def getTweet(user):
+    tweet = ''
+    return tweet
+
+
+# 2. Take in a tweet, return the most important words
+def getWords(tweet):
+    words = []
+    return words
+
+# 3. Take in words[], return corresponding genre
+def getGenre(words):
+    # open genreKeywords.txt
+    # open tags.txt
+    genre = ''
+    return genre
+
+# 4. Take in genre, return movieID(s)
+def getMovieID(genre):
+    movie = ''
+    return movie
+
+# 5. Take in movieID, return movie title
+def getMovie(ID):
+    movie = ''
+    # open 'ml-latest-small/movies.csv'
+    return movie
+
+
+
